4efcdf922e34b0a69bc94a83db5e80f96aec2945
Sebastian Suarez <ssdible@gmail.com>
2026-08-24 21:20:21 -0300

    Carrito funcionando correctamente

--- js/gestores/gestorCarrito.js
+++ js/gestores/gestorCarrito.js
@@ -54,12 +54,31 @@ export class GestorCarrito {
         return new Respuesta(true, "Producto agregado al Carrito");
     }
 
+    actualizarCantidad(idProducto, nuevaCantidad) {
+        let item = this.buscarItem(idProducto);
+        let producto = new GestorProductos(this.#storage)
+                                    .obtenerPorId(idProducto);
+
+        if (!item) {
+            return new Respuesta(false, "Item no encontrado");
+        }
+
+        if (!producto.tieneStock(nuevaCantidad)) {
+            return new Respuesta(false, "No hay stock suficiente");
+        }
+
+        item.actualizarCantidad(nuevaCantidad);
+        this.guardar();
+        return new Respuesta(true, "Actualizado correctamente");
+    }
+
+
     eliminarProducto(idProducto) {
         let id = Number(idProducto);
         let cantidadAnterior = this.#items.length;
 
         this.#items = this.#items.filter(
-            item => item.producto.id !== id
+            item => item.idProducto !== id
         );
 
         let eliminado = cantidadAnterior !== this.#items.length;
--- js/pages/carrito.js
+++ js/pages/carrito.js
@@ -1,20 +1,50 @@
 import {Storage} from "../gestores/gestorStorage.js";
 import {GestorCarrito} from "../gestores/gestorCarrito.js";
-import {GestorProductos} from "../gestores/gestorProductos.js"
-let storage;
-let gestorCarrito;
-let gestorProductos;
+import {GestorProductos} from "../gestores/gestorProductos.js";
+import { mostrarModal } from "../comun.js";;
 
-document.addEventListener("DOMContentLoaded", function(){
-	storage = new Storage();
-	gestorCarrito = new GestorCarrito(storage);
-	gestorProductos = new GestorProductos(storage);
+let storage = new Storage();
+let gestorCarrito = new GestorCarrito(storage);
+let gestorProductos = new GestorProductos(storage);
 
-	mostrarCarrito();
-});
+let contenedorProductos = document.getElementById("seccionProductos");
+
+inicializar();
+
+function inicializar(){
+    contenedorProductos.addEventListener("click", manejarClickCarrito)
+    mostrarCarrito();
+}
+
+function manejarClickCarrito(evento) {
+    let boton = evento.target.closest("button[data-accion][data-id]");
+
+    if (boton === null) {
+        return;
+    }
+
+    let idProducto = Number(boton.dataset.id);
+    let accion = boton.dataset.accion;
+
+    if (accion === "cambiarCantidad") {
+        let input = document.querySelector(`input[data-id="${idProducto}"]`);
+        let nuevaCantidad = Number(input.value);
+
+        let respuesta = gestorCarrito.actualizarCantidad(idProducto, nuevaCantidad);
+
+        if(!respuesta.exito){
+            mostrarModal(false, respuesta.msj);
+        }
+    }
+
+    if (accion === "eliminar") {
+        gestorCarrito.eliminarProducto(idProducto);
+    }
+
+    mostrarCarrito();
+}
 
 function mostrarCarrito(){
-    let contenedorProductos = document.getElementById("seccionProductos");
     let items = gestorCarrito.obtenerTodos();
 
     if(items.length === 0){
@@ -33,17 +63,22 @@ function mostrarCarrito(){
                 <h3>${producto.nombre}</h3>
                 <p>${producto.descripcion}</p>
                 <input type="number"
-						id="input${producto.id}"
+						data-id="${producto.id}"
                         value="${items[i].cantidad}"
                         min="1"
                         max="${producto.stock}"
-                > 
-				<button class="btn btn-small" type="button" onclick="actualizarCantidad(${producto.id})">OK</button>
-				<h2>$${producto.precio} c/u</h2>
-				<button class="btn btn-secondary" type="button" onclick="eliminarProducto(${producto.id})">Eliminar</button>
+                >
+				<button class="btn btn-small" 
+                        type="button"
+                        data-accion="cambiarCantidad"
+                        data-id="${producto.id}">OK</button>
+				<h2>$${items[i].subtotal}</h2>
+				<button class="btn btn-secondary" 
+                        type="button" 
+                        data-accion="eliminar" 
+                        data-id="${producto.id}">Eliminar</button>
             </div>
         `
-		
     }
 	mostrarMontos();
 }
@@ -52,4 +87,8 @@ function mostrarMontos() {
 	document.getElementById("subtotal").innerText = gestorCarrito.calcularSubtotal().toFixed(2);
     document.getElementById("iva").innerText = gestorCarrito.calcularIVA().toFixed(2);
     document.getElementById("total").innerText = gestorCarrito.calcularTotal().toFixed(2);
+}
+
+function actualizarCantidad(){
+    
 }
